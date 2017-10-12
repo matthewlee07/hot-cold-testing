e7cf1fdef2e1f28e2185218687df1d0e031df959
Matt Lee <matthewlee07@gmail.com>
2017-10-12 14:23:23 -0400

    omitted previous reducer.tests, should be in actions.tests; added 2 reducer tests

--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -6,31 +6,24 @@ import {newGame, makeGuess, toggleInfoModal} from '../actions';
 describe('reducer', () => {
   const guess1 = 1;
   const guess2 = 100;
-  const currentState = {
+  const state = {
     guesses: [],
     feedback: 'Make your guess!',
     correctAnswer: 3,
     showInfoModal: false
 };
-  //enter fail cases
-  it('Should return the guess', () => {
-    const state = reducer(currentState, makeGuess(guess1));
-    expect(state).toEqual({guesses: [guess1],
-      feedback: 'You\'re Hot!',
-      correctAnswer: 3,
-      showInfoModal: false});
+  it('Should set the initial state when nothing is passed in', () => {
+    const state = reducer(undefined, {type: '__UNKNOWN'});
+    expect(state.guesses).toEqual([]);
+    expect(state.feedback).toEqual('Make your guess!');
+    expect(state.correctAnswer).toBeGreaterThanOrEqual(0);
+    expect(state.correctAnswer).toBeLessThanOrEqual(100);
   });
-  
-  it('Should return array of guesses', () => {
-    const secondState = Object.assign({}, currentState, {
-      guesses: [guess1]
-    })
 
-    const state = reducer(secondState, makeGuess(guess2));
-    expect(state).toEqual({guesses: [guess1, guess2],
-      feedback: 'You\'re Ice Cold...',
-      correctAnswer: 3,
-      showInfoModal: false});
+  it('Should return the current state on an unknown action', () => {
+    let currentState = {};
+    const state = reducer(currentState, {type: '__UNKNOWN'});
+    expect(state).toBe(currentState);
   });
+
 });
-
